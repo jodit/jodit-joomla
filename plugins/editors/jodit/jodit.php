feca87e9f68d4f149c5c57ffb1c45f1173147a51
Chupurnov Valeriy <skoder@ya.ru>
2018-04-09 21:44:20 +0500

    New version 3.0.84. Read more https://github.com/xdan/jodit/

--- plugins/editors/jodit/jodit.php
+++ plugins/editors/jodit/jodit.php
@@ -43,7 +43,7 @@ class plgEditorJodit extends JPlugin {
 	 * @since  3.2
 	 */
 	protected $app = null;
-	protected $version = '3.0.83';
+	protected $version = '3.0.84';
 
 	/**
 	 * Initialises the Editor.
