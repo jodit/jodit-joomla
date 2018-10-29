1f9feb2699de68a0164c33231a4840f385838fb0
Valera <v-chupurnov@edadeal.ru>
2018-10-29 21:39:13 +0300

    Fixed https://github.com/xdan/jodit/issues/95

--- administrator/components/com_jodit/jodit.php
+++ administrator/components/com_jodit/jodit.php
@@ -15,15 +15,12 @@ use Joomla\CMS\MVC\Controller\BaseController;
 defined('_JEXEC') or die;
 
 // Access check.
-if (!Factory::getUser()->authorise('core.manage', 'com_jodit'))
-{
+if (!Factory::getUser()->authorise('core.manage', 'com_jodit')) {
 	throw new InvalidArgumentException(Text::_('JERROR_ALERTNOAUTHOR'), 404);
 }
 
 // Require the helper
-require_once JPATH_COMPONENT_ADMINISTRATOR . '/helpers/jodit.php';
 require_once JPATH_COMPONENT_ADMINISTRATOR . '/helpers/autoload.php';
-require_once JPATH_COMPONENT_ADMINISTRATOR . '/helpers/JoditApplication.php';
 
 
 // Execute the task
