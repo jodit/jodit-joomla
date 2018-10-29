8d90ff9d1340e690e059a1bcca83e9ec24113e5e
Valera <v-chupurnov@edadeal.ru>
2018-10-29 21:45:02 +0300

    New version 3.1.14. Read more https://github.com/xdan/jodit/

--- administrator/components/com_jodit/jodit.php
+++ administrator/components/com_jodit/jodit.php
@@ -20,6 +20,7 @@ if (!Factory::getUser()->authorise('core.manage', 'com_jodit')) {
 }
 
 // Require the helper
+require_once JPATH_COMPONENT_ADMINISTRATOR . '/helpers/jodit.php';
 require_once JPATH_COMPONENT_ADMINISTRATOR . '/helpers/autoload.php';
 
 
--- plugins/editors/jodit/jodit.php
+++ plugins/editors/jodit/jodit.php
@@ -43,7 +43,7 @@ class plgEditorJodit extends JPlugin {
 	 * @since  3.2
 	 */
 	protected $app = null;
-	protected $version = '3.1.13';
+	protected $version = '3.1.14';
 
 	/**
 	 * Initialises the Editor.
