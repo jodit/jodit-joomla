a217ae473c25e4756ecee48e8edfe80df2641fc1
Valera <v-chupurnov@edadeal.ru>
2020-02-13 02:09:10 +0300

    New version 3.1.36. Read more https://github.com/xdan/jodit/

--- plugins/editors/jodit/jodit.php
+++ plugins/editors/jodit/jodit.php
@@ -43,7 +43,7 @@ class plgEditorJodit extends JPlugin {
 	 * @since  3.2
 	 */
 	protected $app = null;
-	protected $version = '3.1.35';
+	protected $version = '3.1.36';
 
 	/**
 	 * Initialises the Editor.
