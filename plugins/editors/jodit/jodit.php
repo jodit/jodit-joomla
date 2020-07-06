4c0c64a793b22155f9c2cf793eec61c4d82f06bd
v-chupurnov@gmail.com <VufHeevluCa6>
2020-07-07 00:22:02 +0300

    New version 3.1.40. Read more https://github.com/xdan/jodit/

--- plugins/editors/jodit/jodit.php
+++ plugins/editors/jodit/jodit.php
@@ -43,7 +43,7 @@ class plgEditorJodit extends JPlugin {
 	 * @since  3.2
 	 */
 	protected $app = null;
-	protected $version = '3.1.39';
+	protected $version = '3.1.40';
 
 	/**
 	 * Initialises the Editor.
