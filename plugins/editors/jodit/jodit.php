e648748c14f38b55198a38fbe5bb972caf557550
v-chupurnov@gmail.com <VufHeevluCa6>
2020-12-20 17:15:29 +0300

    New version 3.1.54. Read more https://github.com/xdan/jodit/

--- plugins/editors/jodit/jodit.php
+++ plugins/editors/jodit/jodit.php
@@ -43,7 +43,7 @@ class plgEditorJodit extends JPlugin {
 	 * @since  3.2
 	 */
 	protected $app = null;
-	protected $version = '3.1.53';
+	protected $version = '3.1.54';
 
 	/**
 	 * Initialises the Editor.
