b2d7e08dc815e0da2cae2f39214c7060868028f7
xdan <skoder@ya.ru>
2022-02-07 23:52:37 +0300

    New version 3.1.80. Read more https://github.com/xdan/jodit/

--- plugins/editors/jodit/jodit.php
+++ plugins/editors/jodit/jodit.php
@@ -43,7 +43,7 @@ class plgEditorJodit extends JPlugin {
 	 * @since  3.2
 	 */
 	protected $app = null;
-	protected $version = '3.1.79';
+	protected $version = '3.1.80';
 
 	/**
 	 * Initialises the Editor.
